c0aa1d41aac35883371e4556df1affa661c91f1d
Bhagirath Bhatt <budbhatt@gmail.com>
2020-11-13 19:29:26 -0800

    first commit

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,74 @@
+class Node:
+    """
+    This is the Node class
+    """
+
+    def __init__(self, value, next_node = None):
+        """
+        This is to create a node with a value and a next reference
+
+        Args:
+            value : node value
+            next_node : reference to next node
+        """
+
+        self.value = value
+        self.next_node = next_node
+
+    def __repr__ (self):
+        return {'Value':{self.value}, "Next Node":{self.next.node}}
+
+
+    def __str__(self):
+        return f"{self.value}, 'Next Node':{self.next.node}"
+
+
 class LinkedList:
     """
-    Put docstring here
+    This is the class LinkedList
     """
-
     def __init__(self):
-        # initialization here
-        pass
+        """
+        This is t initialize LinkedList
+        """
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+
+    def __str__(self):
+        """
+        prints LinkedList
+        """
+        return f"head: {self.head}"
+
+    def insert(self, value):
+        """
+        Method to insert a node to the LinkedList.
+        """
+        node = Node(value)
+        
+        if self.head is not None:
+            node.next_node = self.head
+        self.head = node
+
+    
+    def includes(self, value):
+        """
+        Method to check in a Linked List includes a value.
+        """
+        current = self.head
+
+        while current is not None:
+            if current.value == value:
+                return True
+            current = current.next_node
+        
+        return False
+
+
+
+
+
+
+
+
+
